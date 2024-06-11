2bee5d16b29b51ce1892090427442c7b6b9dc7cf
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-06-11 10:41:32 -0400

    formatting

--- src/components/OperationComponent.tsx
+++ src/components/OperationComponent.tsx
@@ -16,14 +16,14 @@ interface OperationProps {
     filterQuery: string;
 }
 
-const OperationComponent: React.FC<OperationProps> = ({op, filterQuery}) => {
+const OperationComponent: React.FC<OperationProps> = ({ op, filterQuery }) => {
     return (
         <div className='operation-component'>
-            <Icon className='operation-icon' size={20} icon={IconNames.CUBE}/>
-            <HighlightedText text={op.name} filter={filterQuery}/>
-            <Button title='Operation tensor report' minimal small icon={IconNames.GRAPH}/>
-            <Button title='Stack trace' minimal small icon={IconNames.CODE}/>
-            <Button title='Buffer view' minimal small icon={IconNames.SEGMENTED_CONTROL}/>
+            <Icon className='operation-icon' size={20} icon={IconNames.CUBE} />
+            <HighlightedText text={op.name} filter={filterQuery} />
+            <Button title='Operation tensor report' minimal small icon={IconNames.GRAPH} />
+            <Button title='Stack trace' minimal small icon={IconNames.CODE} />
+            <Button title='Buffer view' minimal small icon={IconNames.SEGMENTED_CONTROL} />
             {/* <GoldenTensorComparisonIndicator value={op.goldenGlobal} /> */}
             {/* <GoldenTensorComparisonIndicator value={op.goldenLocal} /> */}
         </div>
