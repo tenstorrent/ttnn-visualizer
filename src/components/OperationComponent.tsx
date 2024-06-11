1b94f903b1c026cb3c2ab70da1bc7fd18059bb86
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-06-11 10:41:00 -0400

    PR feedback tweaks

--- src/components/OperationComponent.tsx
+++ src/components/OperationComponent.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 import { Button, Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
-import HighlightedText from './HighlightedText'; // Assuming you have this component
+import HighlightedText from './HighlightedText';
 import '../scss/components/Operation.scss';
 
 interface OperationProps {
@@ -16,16 +16,14 @@ interface OperationProps {
     filterQuery: string;
 }
 
-const OperationComponent: React.FC<OperationProps> = ({ op, filterQuery }) => {
+const OperationComponent: React.FC<OperationProps> = ({op, filterQuery}) => {
     return (
         <div className='operation-component'>
-            <Icon className='operation-icon' size={20} icon={IconNames.CUBE} />
-            <span style={{ color: '#fff', fontSize: '20px' }}>
-                <HighlightedText text={op.name} filter={filterQuery} />
-            </span>
-            <Button title='Operation tensor report' minimal small icon={IconNames.GRAPH} />
-            <Button title='Stack trace' minimal small icon={IconNames.CODE} />
-            <Button title='Buffer view' minimal small icon={IconNames.SEGMENTED_CONTROL} />
+            <Icon className='operation-icon' size={20} icon={IconNames.CUBE}/>
+            <HighlightedText text={op.name} filter={filterQuery}/>
+            <Button title='Operation tensor report' minimal small icon={IconNames.GRAPH}/>
+            <Button title='Stack trace' minimal small icon={IconNames.CODE}/>
+            <Button title='Buffer view' minimal small icon={IconNames.SEGMENTED_CONTROL}/>
             {/* <GoldenTensorComparisonIndicator value={op.goldenGlobal} /> */}
             {/* <GoldenTensorComparisonIndicator value={op.goldenLocal} /> */}
         </div>
