8c9b52172e351bd637b3c877bb2df264ab01b481
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-13 12:13:27 -0400

    Renamed App to Home and moved to roots

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './App';
 import './index.scss';
+import OperationView from './roots/OperationView';
+import OperationList from './components/OperationList';
+import ErrorPage from './error-page';
+import Layout from './components/Layout';
+import Home from './roots/Home';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: 'operations',
+                element: <OperationList />,
+            },
+            {
+                path: 'operations/:opId',
+                loader: () => ({ message: "You're interested in viewing the following operation" }),
+                element: <OperationView />,
+            },
+        ],
+    },
+]);
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
         <React.StrictMode>
-            <App />
+            <RouterProvider router={router} />
         </React.StrictMode>
     </QueryClientProvider>,
 );
---
+++ src/roots/Home.tsx
@@ -0,0 +1,7 @@
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return <Link to='/operations'>View operations</Link>;
+}
+
+export default Home;
