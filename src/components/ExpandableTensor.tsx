db90cdf2193e12be671abad5411ce98bf20cdd91
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-17 09:38:49 -0400

    Added styling

--- src/components/ExpandableTensor.tsx
+++ src/components/ExpandableTensor.tsx
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 import { Switch } from '@blueprintjs/core';
 import { useState } from 'react';
+import 'styles/components/ExpandableTensor.scss';
 
 interface ExpandableTensorProps {
     tensor: string;
@@ -24,6 +25,7 @@ function ExpandableTensor({ tensor }: ExpandableTensorProps) {
                 </>
             )}
             <Switch
+                className='expandable-tensor'
                 label={isExpanded ? 'Hide full tensor' : 'Show full tensor'}
                 onChange={() => setIsExpanded((previousValue) => !previousValue)}
                 checked={isExpanded}
