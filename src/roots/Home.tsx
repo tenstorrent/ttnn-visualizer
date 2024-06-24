6c1bb27cbf2354a712aaada7ffe85a4a4d095103
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-24 09:47:45 -0400

    Added homescreen and updated packages

--- src/roots/Home.tsx
+++ src/roots/Home.tsx
@@ -1,7 +1,34 @@
+import { Icon } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import { Link } from 'react-router-dom';
+import LocalFolderSelector from '../components/LocalFolderSelector';
+import RemoteSyncConfigurator from '../components/RemoteSyncConfigurator';
 
 function Home() {
-    return <Link to='/operations'>View operations</Link>;
+    return (
+        <>
+            <Link to='/operations'>View operations</Link>
+
+            <div className='splash-screen'>
+                <div className='folder-picker-options'>
+                    <fieldset>
+                        <legend>Local folder</legend>
+                        <Icon icon={IconNames.FOLDER_OPEN} size={150} />
+                        <div className='folder-picker-wrapper'>
+                            <LocalFolderSelector />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <legend>Remote Sync</legend>
+                        <Icon icon={IconNames.CLOUD} size={150} />
+                        <div className='folder-picker-wrapper'>
+                            <RemoteSyncConfigurator />
+                        </div>
+                    </fieldset>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
