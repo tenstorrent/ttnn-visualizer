872f535aaa979ca2bcb1ad169e8101483a5efc2f
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-25 15:24:28 -0400

    Added BP dark theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,42 @@
-import { Link, Outlet } from 'react-router-dom';
-import { FocusStyleManager } from '@blueprintjs/core';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Button, FocusStyleManager, Intent } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import TenstorrentLogo from './TenstorrentLogo';
 
 function Layout() {
     FocusStyleManager.onlyShowFocusOnTabs();
+    const navigate = useNavigate();
+    const navigation = useLocation();
+
+    const handleNavigate = (path: string) => {
+        navigate(path);
+    };
 
     return (
-        <>
+        <div className='bp5-dark'>
             <header className='app-header'>
                 <Link to='/'>
                     <TenstorrentLogo />
                 </Link>
+
+                {/* TODO: Handle navigation variations differently */}
+                {navigation.pathname !== '/' && (
+                    <nav>
+                        <Button
+                            icon={IconNames.FOLDER_SHARED_OPEN}
+                            onClick={() => handleNavigate('/')}
+                            intent={Intent.PRIMARY}
+                        >
+                            Select new report
+                        </Button>
+                    </nav>
+                )}
             </header>
 
             <main>
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 }
 
