9215f1677573d0570322a749bd7300a6ccf3c487
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-26 10:20:58 -0400

    Added report title styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,12 @@ import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Button } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import TenstorrentLogo from './TenstorrentLogo';
+import { useReport } from '../hooks/useAPI';
 
 function Layout() {
     const navigate = useNavigate();
     const location = useLocation();
+    const report = useReport();
 
     const handleNavigate = (path: string) => {
         navigate(path);
@@ -20,11 +22,15 @@ function Layout() {
 
                 {/* TODO: Handle navigation variations differently */}
                 {location.pathname !== '/' && (
-                    <nav>
-                        <Button minimal icon={IconNames.FOLDER_SHARED_OPEN} onClick={() => handleNavigate('/')}>
-                            Select report
-                        </Button>
-                    </nav>
+                    <>
+                        <span className='report-title'>{report.report_name}</span>
+
+                        <nav>
+                            <Button minimal icon={IconNames.FOLDER_SHARED_OPEN} onClick={() => handleNavigate('/')}>
+                                Select report
+                            </Button>
+                        </nav>
+                    </>
                 )}
             </header>
 
