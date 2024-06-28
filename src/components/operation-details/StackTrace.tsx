aada9e798e38bfc6b91c89b0f21f3651feea8dda
David Blundell <david.blundell@lightsonsoftware.com>
2024-06-28 13:55:39 -0400

    Committing current state

--- src/components/operation-details/StackTrace.tsx
+++ src/components/operation-details/StackTrace.tsx
@@ -1,9 +1,10 @@
 // Using ES6 import syntax
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
-import { useEffect, useMemo } from 'react';
+import { ButtonHTMLAttributes, HTMLAttributes, useMemo, useRef } from 'react';
 import 'highlight.js/styles/a11y-dark.css';
 import 'styles/components/StackTrace.scss';
+import { Button } from '@blueprintjs/core';
 
 // Then register the languages you need
 hljs.registerLanguage('python', python);
@@ -12,22 +13,41 @@ interface StackTraceProps {
     stackTrace: string;
 }
 
+type PopoverButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    popovertarget: string;
+};
+
+type PopoverDivProps = HTMLAttributes<HTMLDivElement> & {
+    popover: boolean;
+    id: string;
+};
+
 function StackTrace({ stackTrace }: StackTraceProps) {
+    const popoverRef = useRef(null);
     const highlightedCode = useMemo(() => hljs.highlight(stackTrace, { language: 'python' }), [stackTrace]);
 
-    useEffect(() => {
-        hljs.highlightAll();
-    }, []);
+    const handlePopoverReveal = () => {
+        if (popoverRef?.current) {
+            const el: HTMLElement = popoverRef.current;
+            el.togglePopover();
+        }
+    };
 
     return (
-        // <>
-        //     <button className='bp5-button' type='button' popovertarget='wave'>
-        //         View stack trace
-        //     </button>
-        <pre id='wave='>
-            <code className='language-python' dangerouslySetInnerHTML={{ __html: highlightedCode.value }} />
-        </pre>
-        // </>
+        <>
+            <Button className='bp5-button' type='button' onClick={handlePopoverReveal}>
+                View stack trace
+            </Button>
+            <div id='wave' popover='' ref={popoverRef}>
+                <pre>
+                    <code
+                        className='language-python'
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{ __html: highlightedCode.value }}
+                    />
+                </pre>
+            </div>
+        </>
     );
 }
 
