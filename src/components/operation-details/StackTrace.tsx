196ade1bcb475859a74a88911a3a996499cf840f
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-03 10:40:12 -0400

    Styling adjusted

--- src/components/operation-details/StackTrace.tsx
+++ src/components/operation-details/StackTrace.tsx
@@ -17,21 +17,12 @@ function StackTrace({ stackTrace }: StackTraceProps) {
     const highlightedCode = useMemo(() => hljs.highlight(stackTrace, { language: 'python' }), [stackTrace]);
 
     return (
-        <pre className='code-output-summary'>
-            <Button
-                className='show-full-stack-trace bp5-button'
-                type='button'
-                minimal
-                intent={Intent.PRIMARY}
-                onClick={() => setIsFullStackTrace(!isFullStackTrace)}
-            >
-                Show full stack trace
-            </Button>
-
+        <pre className='stack-trace'>
             {isFullStackTrace ? (
                 <Collapse
                     isOpen={isFullStackTrace}
                     keepChildrenMounted={false}
+                    className='code-wrapper'
                 >
                     <code
                         className='language-python code-output'
@@ -40,12 +31,24 @@ function StackTrace({ stackTrace }: StackTraceProps) {
                     />
                 </Collapse>
             ) : (
-                <code
-                    className='language-python code-output'
-                    // eslint-disable-next-line react/no-danger
-                    dangerouslySetInnerHTML={{ __html: `File${highlightedCode.value.split('File')[1]}` }}
-                />
+                <div className='code-wrapper'>
+                    <code
+                        className='language-python code-output'
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{ __html: `  File${highlightedCode.value.split('File')[1]}` }}
+                    />
+                </div>
             )}
+
+            <Button
+                className='show-full-stack-trace'
+                type='button'
+                minimal
+                intent={Intent.PRIMARY}
+                onClick={() => setIsFullStackTrace(!isFullStackTrace)}
+            >
+                {isFullStackTrace ? 'Hide full stack trace' : 'Show full stack trace'}
+            </Button>
         </pre>
     );
 }
