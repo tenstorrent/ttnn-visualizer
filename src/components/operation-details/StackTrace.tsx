82f3abf315078c0d3e6c2695f1c6c37ff5e399e0
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-03 09:47:28 -0400

    Moved to collapsible

--- src/components/operation-details/StackTrace.tsx
+++ src/components/operation-details/StackTrace.tsx
@@ -1,10 +1,10 @@
 // Using ES6 import syntax
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
-import { useMemo, useRef } from 'react';
+import { useMemo, useState } from 'react';
 import 'highlight.js/styles/a11y-dark.css';
 import 'styles/components/StackTrace.scss';
-import { Button, Intent } from '@blueprintjs/core';
+import { Button, Collapse, Intent } from '@blueprintjs/core';
 
 hljs.registerLanguage('python', python);
 
@@ -13,52 +13,40 @@ interface StackTraceProps {
 }
 
 function StackTrace({ stackTrace }: StackTraceProps) {
-    const popoverRef = useRef(null);
+    const [isFullStackTrace, setIsFullStackTrace] = useState(false);
     const highlightedCode = useMemo(() => hljs.highlight(stackTrace, { language: 'python' }), [stackTrace]);
 
-    const handlePopoverReveal = () => {
-        if (popoverRef?.current) {
-            const el: HTMLElement = popoverRef.current;
-            el.togglePopover();
-        }
-    };
-
-    console.log(highlightedCode.value);
-
     return (
-        <>
-            <pre className='code-output-summary'>
-                <code
-                    className='language-python code-output'
-                    // eslint-disable-next-line react/no-danger
-                    dangerouslySetInnerHTML={{ __html: `File${highlightedCode.value.split('File')[1]}` }}
-                />
-
-                <Button
-                    className='bp5-button'
-                    type='button'
-                    onClick={handlePopoverReveal}
-                    minimal
-                    intent={Intent.PRIMARY}
-                >
-                    Show full stack trace
-                </Button>
-            </pre>
-
-            <div
-                id='wave'
-                popover=''
-                ref={popoverRef}
+        <pre className='code-output-summary'>
+            <Button
+                className='show-full-stack-trace bp5-button'
+                type='button'
+                minimal
+                intent={Intent.PRIMARY}
+                onClick={() => setIsFullStackTrace(!isFullStackTrace)}
             >
-                <pre>
+                Show full stack trace
+            </Button>
+
+            {isFullStackTrace ? (
+                <Collapse
+                    isOpen={isFullStackTrace}
+                    keepChildrenMounted={false}
+                >
                     <code
                         className='language-python code-output'
                         // eslint-disable-next-line react/no-danger
                         dangerouslySetInnerHTML={{ __html: highlightedCode.value }}
                     />
-                </pre>
-            </div>
-        </>
+                </Collapse>
+            ) : (
+                <code
+                    className='language-python code-output'
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{ __html: `File${highlightedCode.value.split('File')[1]}` }}
+                />
+            )}
+        </pre>
     );
 }
 
