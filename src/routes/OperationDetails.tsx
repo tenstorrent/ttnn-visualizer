0b531491ddf270459463f76e37dcf49b39a8e17f
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-08 12:01:59 -0400

    Moved stack track logic and navigation into OperationDetailsComponent

--- src/routes/OperationDetails.tsx
+++ src/routes/OperationDetails.tsx
@@ -1,26 +1,9 @@
 import { useParams } from 'react-router';
-import { useState } from 'react';
 import OperationDetailsComponent from '../components/operation-details/OperationDetailsComponent';
-import OperationDetailsNavigation from '../components/OperationDetailsNavigation';
 
 export default function OperationDetails() {
-    const [isFullStackTrace, setIsFullStackTrace] = useState(false);
     const { operationId } = useParams();
     const intOperationId = operationId ? parseInt(operationId, 10) : 0;
 
-    return (
-        operationId && (
-            <>
-                <OperationDetailsNavigation
-                    operationId={intOperationId}
-                    isFullStackTrace={isFullStackTrace}
-                />
-                <OperationDetailsComponent
-                    operationId={intOperationId}
-                    isFullStackTrace={isFullStackTrace}
-                    toggleStackTraceHandler={setIsFullStackTrace}
-                />
-            </>
-        )
-    );
+    return operationId && <OperationDetailsComponent operationId={intOperationId} />;
 }
