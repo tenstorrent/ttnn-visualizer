6bc72ff8ffc3bcd2cf6754383a81748b36f45683
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-12 15:56:21 -0400

    Added licencing message

--- src/components/report-selection/ConnectionTestMessage.tsx
+++ src/components/report-selection/ConnectionTestMessage.tsx
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
 import { IconName, IconNames } from '@blueprintjs/icons';
 import { Icon, Intent } from '@blueprintjs/core';
 import { ConnectionTestStates } from '../../hooks/useRemote';
