99cde808ccceab9b99294db9cd696ced720a622c
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-19 11:07:38 -0400

    Moved FolderPicker logic into LocalFolderSelector

--- src/components/report-selection/LocalFolderSelector.tsx
+++ src/components/report-selection/LocalFolderSelector.tsx
@@ -2,16 +2,70 @@
 //
 // SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
-import { FormGroup } from '@blueprintjs/core';
-import { type FC } from 'react';
-import { useNavigate } from 'react-router';
-import FolderPicker from './FolderPicker';
+import { Button, FormGroup, Icon, IconName, Intent } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
+import { type FC, useState } from 'react';
 
 import 'styles/components/FolderPicker.scss';
-import ROUTES from '../../definitions/routes';
+import { FileWithDirectoryAndFileHandle, directoryOpen } from 'browser-fs-access';
+import { ConnectionStatus, ConnectionTestStates } from '../../model/Connection';
+
+const OPEN_FOLDER_OPTIONS = {
+    recursive: false,
+    id: 'projects',
+};
+
+interface LocalFile extends FileWithDirectoryAndFileHandle {
+    webkitRelativePath: string;
+}
+
+const ICON_MAP: Record<ConnectionTestStates, IconName> = {
+    [ConnectionTestStates.IDLE]: IconNames.DOT,
+    [ConnectionTestStates.PROGRESS]: IconNames.DOT,
+    [ConnectionTestStates.FAILED]: IconNames.CROSS,
+    [ConnectionTestStates.OK]: IconNames.TICK,
+};
+
+const INTENT_MAP: Record<ConnectionTestStates, Intent> = {
+    [ConnectionTestStates.IDLE]: Intent.NONE,
+    [ConnectionTestStates.PROGRESS]: Intent.WARNING,
+    [ConnectionTestStates.FAILED]: Intent.DANGER,
+    [ConnectionTestStates.OK]: Intent.SUCCESS,
+};
 
 const LocalFolderOptions: FC = () => {
-    const navigate = useNavigate();
+    const [status, setStatus] = useState<ConnectionTestStates | undefined>();
+    const [message, setMessage] = useState<string | undefined>();
+
+    const handleDirectoryOpen = async () => {
+        const files = (await directoryOpen(OPEN_FOLDER_OPTIONS)) as LocalFile[];
+        const connectionStatus: ConnectionStatus = {
+            status: ConnectionTestStates.FAILED,
+            message: 'Some kind of error with this',
+        };
+
+        // TODO: Confirm this is the correct way to POST files
+        // let response = await axios.post('/api/local/directory', files, {
+        //     headers: {
+        //         'Content-Type': 'multipart/form-data',
+        //     },
+        // });
+
+        const response = {
+            status: 200,
+            message: 'all good chum',
+        };
+
+        if (response.status === 200) {
+            connectionStatus.status = ConnectionTestStates.OK;
+            connectionStatus.message = 'Files uploaded successful';
+        }
+
+        // TODO: Handle other errors more betterly
+
+        setStatus(connectionStatus.status);
+        setMessage(connectionStatus.message);
+    };
 
     return (
         <FormGroup
@@ -20,7 +74,23 @@ const LocalFolderOptions: FC = () => {
             subLabel='Select a local directory containing a report'
         >
             <div className='buttons-container'>
-                <FolderPicker onSelectFolder={() => navigate(ROUTES.OPERATIONS)} />
+                <Button
+                    onClick={handleDirectoryOpen}
+                    icon={IconNames.FOLDER_OPEN}
+                >
+                    Open Directory
+                </Button>
+                {status && message && (
+                    <div className={`verify-connection-item status-${ConnectionTestStates[status]}`}>
+                        <Icon
+                            className='connection-status-icon'
+                            icon={ICON_MAP[status]}
+                            size={20}
+                            intent={INTENT_MAP[status]}
+                        />
+                        <span className='connection-status-text'>{message}</span>
+                    </div>
+                )}
             </div>
         </FormGroup>
     );
