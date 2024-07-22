22903291d75b4a60c9a2baa9c162b64f059b2d23
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-22 15:53:00 -0400

    Added basic microop rendering

--- src/components/OperationArguments.tsx
+++ src/components/OperationArguments.tsx
@@ -4,6 +4,8 @@
 import 'styles/components/OperationArguments.scss';
 import { ScrollToOptions } from '@tanstack/react-virtual';
 import ExpandableTensor from './ExpandableTensor';
+import OperationHistory from '../definitions/operationHistory.json';
+import MicroOperations from './MicroOperations';
 
 interface Arguments {
     name: string;
@@ -13,31 +15,38 @@ interface Arguments {
 interface OperationArgumentsProps {
     operationId: number;
     operationIndex: number;
-    data: Array<Arguments>;
+    argumentsData: Array<Arguments>;
     scrollTo: (index: number, { align, behavior }: ScrollToOptions) => void;
 }
 
-function OperationArguments({ operationId, operationIndex, data, scrollTo }: OperationArgumentsProps) {
+function OperationArguments({ operationId, operationIndex, argumentsData, scrollTo }: OperationArgumentsProps) {
+    const microOperations = OperationHistory.filter((microOp) => microOp.ttnn_operation_id === operationId);
+
     return (
-        <table className='operation-arguments'>
-            <caption>Arguments</caption>
-            <tbody>
-                {data?.map((arg) => (
-                    <tr key={`${operationId}-${arg.name}`}>
-                        <td>{arg.name}</td>
-                        {isLengthyTensor(arg.value) ? (
-                            <ExpandableTensor
-                                tensor={arg.value}
-                                operationIndex={operationIndex}
-                                scrollTo={scrollTo}
-                            />
-                        ) : (
-                            <td>{arg.value}</td>
-                        )}
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            <table className='operation-arguments'>
+                <caption>Arguments</caption>
+
+                <tbody>
+                    {argumentsData?.map((arg) => (
+                        <tr key={`${operationId}-${arg.name}`}>
+                            <td>{arg.name}</td>
+                            {isLengthyTensor(arg.value) ? (
+                                <ExpandableTensor
+                                    tensor={arg.value}
+                                    operationIndex={operationIndex}
+                                    scrollTo={scrollTo}
+                                />
+                            ) : (
+                                <td>{arg.value}</td>
+                            )}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {microOperations?.length ? <MicroOperations microOperations={microOperations} /> : null}
+        </>
     );
 }
 
