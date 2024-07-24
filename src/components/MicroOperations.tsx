f86d29450d789e6ef9b4c1d62432edfcd1a9c7e9
David Blundell <david.blundell@lightsonsoftware.com>
2024-07-24 10:01:43 -0400

    Added more information

--- src/components/MicroOperations.tsx
+++ src/components/MicroOperations.tsx
@@ -1,10 +1,23 @@
+interface MemoryConfig {
+    buffer_type: number;
+    memory_layout: number;
+    shard_spec: number | null;
+}
+
+interface InputTensor {
+    dtype: number;
+    layout: number;
+    memory_config: MemoryConfig;
+}
+
 interface MicroOperation {
-    composite_parent_names: string[];
-    input_tensor_records: object;
+    composite_parent_names: [];
+    input_tensor_records: InputTensor[];
     operation_name: string;
     operation_type: string;
     program_cache_hit: boolean | null;
     program_hash: number | null;
+    ttnn_operation_id: number;
 }
 
 interface MicroOperationsData {
@@ -22,14 +35,57 @@ function MicroOperations({ microOperations }: MicroOperationsData) {
                     <td>Operation type</td>
                     <td>Program cache hit</td>
                     <td>Program hash</td>
+                    <td>Attributes</td>
                 </tr>
 
                 {microOperations.map((op) => (
-                    <tr>
+                    <tr key={op.program_hash}>
                         <td>{op.operation_name}</td>
                         <td>{op.operation_type}</td>
-                        <td>{op.program_cache_hit?.toString()}</td>
-                        <td>{op.program_hash}</td>
+                        <td>{op.program_cache_hit?.toString() ?? 'null'}</td>
+                        <td>{op.program_hash ?? 'null'}</td>
+                        <td>
+                            {op.input_tensor_records.map((inputTensor, index) => (
+                                // eslint-disable-next-line react/no-array-index-key
+                                <table key={`${op.program_hash}-${index}`}>
+                                    <tbody>
+                                        <tr>
+                                            <td>Dtype</td>
+                                            <td>{inputTensor.dtype}</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Layout</td>
+                                            <td>{inputTensor.layout}</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Memory config</td>
+                                            <td>
+                                                <table>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Buffer type</td>
+                                                            <td>{inputTensor.memory_config.buffer_type}</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Memory layout</td>
+                                                            <td>{inputTensor.memory_config.memory_layout}</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>Shard spec</td>
+                                                            <td>{inputTensor.memory_config.shard_spec ?? 'null'}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            ))}
+                        </td>
                     </tr>
                 ))}
             </tbody>
