217a744b5b1e9e5143d689aea47387072c6ba8b3
Greg Hatt <greg@lightsonsoftware.com>
2024-08-14 14:44:49 -0300

    Use autocommit since we're not writing to database

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,7 +1,16 @@
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_static_digest import FlaskStaticDigest
-from flask_marshmallow import Marshmallow
 
-db = SQLAlchemy()
+
+class SQLiteAlchemy(SQLAlchemy):
+    def apply_driver_hacks(self, app, info, options):
+        options.update({
+            'isolation_level': 'AUTOCOMMIT',
+        })
+        super(SQLiteAlchemy, self).apply_driver_hacks(app, info, options)
+
+
+db = SQLiteAlchemy()
 flask_static_digest = FlaskStaticDigest()
 ma = Marshmallow()
