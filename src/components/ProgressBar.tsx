31d848278630ef10378479750124b3c81bd78ca0
David Blundell <david.blundell@lightsonsoftware.com>
2024-08-23 14:57:52 -0400

    Fixed issue with progress bar outputting a 0

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -5,13 +5,13 @@ import { UploadProgress } from '../hooks/useLocal';
 function ProgressBar({ progress, estimated }: UploadProgress) {
     return (
         <div className='progress-bar'>
-            {progress && estimated && (
+            {progress && estimated ? (
                 <span className='status'>
                     {progress > 0 ? `${Math.round(progress * 100)}%` : `100%`}
                     {` - `}
                     {estimated > 0 ? `${Math.round(estimated)}s left` : '0s left'}
                 </span>
-            )}
+            ) : null}
 
             <BlueprintProgressBar value={progress} />
         </div>
