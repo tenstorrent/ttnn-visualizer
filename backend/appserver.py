548564bf7d6f0d4bf763c11d7f0457f652f3b491
Greg Hatt <greg@lightsonsoftware.com>
2024-09-12 16:41:27 -0300

    Use package name in serve command

--- backend/appserver.py
+++ backend/appserver.py
@@ -1,15 +1,18 @@
 # serve.py
 import sys
 from gunicorn.app.wsgiapp import run
+import pathlib
 
+app_dir = pathlib.Path(__file__).parent.resolve()
+config_dir = app_dir.joinpath('config')
 
 def serve():
     # Prepare arguments for Gunicorn
     sys.argv = [
         "gunicorn",
         "-c",
-        "backend/config/gunicorn.py",
+        str(config_dir.joinpath('gunicorn.py').absolute()),
         # "-k", "uvicorn.workers.UvicornWorker",
-        "backend.app:create_app()",
+        "ttnn_visualizer.app:create_app()",
     ]
     run()
