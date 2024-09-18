599ace5dec481f1431d2edceb653efc80e1854ef
Greg Hatt <greg@lightsonsoftware.com>
2024-09-17 21:49:30 -0300

    Change number of workers/threads since no multithreaded use cases exist currently

--- backend/config/gunicorn.py
+++ backend/config/gunicorn.py
@@ -11,14 +11,17 @@ from dotenv import load_dotenv
 dotenv_path = Path(__file__).parent.parent.parent.joinpath('.env')
 print(f"Looking for .env at {dotenv_path}")
 if dotenv_path.exists():
-    print('Loading .env file')
     load_dotenv(str(dotenv_path))
 
 bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 accesslog = "-"
 access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
 
-workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
-threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
+workers = 1
+threads = 1
+
+# Currently no need for multithreading/workers
+# workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
+# threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
 
 reload = bool(str_to_bool(os.getenv("WEB_RELOAD", "false")))
