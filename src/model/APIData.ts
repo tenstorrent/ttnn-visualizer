07f6f7e9af5e4ffd7c15178ca0bc65718f909dbb
Greg Hatt <greg@lightsonsoftware.com>
2024-10-15 13:23:27 -0300

    Adds interface for API data for file progress

--- src/model/APIData.ts
+++ src/model/APIData.ts
@@ -45,6 +45,24 @@ export interface ActiveReport {
     remote_connection?: RemoteConnection;
 }
 
+export enum FileStatus {
+    DOWNLOADING = "DOWNLOADING",
+    FAILED = "FAILED",
+    COMPRESSING = "COMPRESSING",
+    FINISHED = "FINISHED",
+    STARTED = "STARTED"
+}
+
+// TypeScript Interface with underscored keys to match the backend data
+export interface FileProgress {
+    current_file_name: string;
+    number_of_files: number;
+    percent_of_current: number,
+    finished_files: number;
+    status: FileStatus;  // Use the FileStatus enum
+    timestamp?: string;  // Optional, with default handled elsewhere if necessary
+}
+
 // TODO: we may want to revisit the 'default' portion for the variable name
 export const defaultOperationDetailsData: OperationDetailsData = {
     id: 0,
