cd43be1e486e06321d86690d8b57c61cc4377ef8
Greg Hatt <greg@lightsonsoftware.com>
2024-10-15 13:20:31 -0300

    Use singleton for app config in app.py
    - Updates run command for Flask
    - Adds socketio to UI dependencies
    - Uses gunicorn environment variables in run command

--- backend/ttnn_visualizer/app.py
+++ backend/ttnn_visualizer/app.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess
 from os import environ
 from pathlib import Path
 
@@ -9,7 +10,7 @@ from flask_cors import CORS
 from werkzeug.debug import DebuggedApplication
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from ttnn_visualizer import settings
+from ttnn_visualizer.settings import Config
 
 
 def create_app(settings_override=None):
@@ -31,7 +32,7 @@ def create_app(settings_override=None):
 
     app = Flask(__name__, static_folder=static_assets_dir, static_url_path="/")
 
-    app.config.from_object(getattr(settings, flask_env))
+    app.config.from_object(Config())
 
     logging.basicConfig(level=app.config.get("LOG_LEVEL", "INFO"))
 
@@ -40,6 +41,7 @@ def create_app(settings_override=None):
     if settings_override:
         app.config.update(settings_override)
 
+    app.config["USE_WEBSOCKETS"] = True  # Set this based on environment
     middleware(app)
 
     app.register_blueprint(api)
@@ -57,7 +59,8 @@ def create_app(settings_override=None):
 
 
 def extensions(app: flask.Flask):
-    from ttnn_visualizer.extensions import flask_static_digest, db, session
+    from ttnn_visualizer.extensions import flask_static_digest, db, session, socketio
+    from ttnn_visualizer.sockets import register_handlers
 
     """
     Register 0 or more extensions (mutates the app passed in).
@@ -67,16 +70,22 @@ def extensions(app: flask.Flask):
     """
 
     flask_static_digest.init_app(app)
+    socketio.init_app(app)
     db.init_app(app)
 
     app.config["SESSION_TYPE"] = "sqlalchemy"
     app.config["SESSION_SQLALCHEMY"] = db
 
+    with app.app_context():
+        db.drop_all()
+
     session.init_app(app)
+    # Register the handlers by passing the socketio instance
+
+    register_handlers(socketio)
 
     # Create the tables within the application context
     with app.app_context():
-        db.drop_all()
         db.create_all()
 
     # For automatically reflecting table data
@@ -93,12 +102,14 @@ def middleware(app: flask.Flask):
     :param app: Flask application instance
     :return: None
     """
-    # Enable the Flask interactive debugger in the broswer for development.
-    if app.debug:
-        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+    # Only use the middleware if running in pure WSGI (HTTP requests)
+    if not app.config.get("USE_WEBSOCKETS"):
+        # Enable the Flask interactive debugger in the browser for development.
+        if app.debug:
+            app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
-    # Set the real IP address into request.remote_addr when behind a proxy.
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+        # Set the real IP address into request.remote_addr when behind a proxy.
+        app.wsgi_app = ProxyFix(app.wsgi_app)
 
     # CORS configuration
     origins = ["http://localhost:5173", "http://localhost:8000"]
@@ -109,3 +120,20 @@ def middleware(app: flask.Flask):
     )
 
     return None
+
+
+if __name__ == "__main__":
+    config = Config()
+
+    gunicorn_args = [
+        "gunicorn",
+        "-k",
+        config.GUNICORN_WORKER_CLASS,
+        "-w",
+        config.GUNICORN_WORKERS,
+        config.GUNICORN_APP_MODULE,
+        "-b",
+        config.GUNICORN_BIND,
+    ]
+
+    subprocess.run(gunicorn_args)
--- backend/ttnn_visualizer/appserver.py
+++ backend/ttnn_visualizer/appserver.py
@@ -4,18 +4,29 @@ import sys
 
 from gunicorn.app.wsgiapp import run
 
+from ttnn_visualizer.settings import Config
+
 app_dir = pathlib.Path(__file__).parent.resolve()
 config_dir = app_dir.joinpath("config")
 static_assets_dir = app_dir.joinpath("static")
 
 
 def serve():
+    """Run command for use in wheel package entrypoint"""
+    config = Config()
+
     os.environ.setdefault("FLASK_ENV", "production")
     os.environ.setdefault("STATIC_ASSETS", str(static_assets_dir))
+
     sys.argv = [
         "gunicorn",
-        "-c",
-        str(config_dir.joinpath("gunicorn.py").absolute()),
-        "ttnn_visualizer.app:create_app()",
+        "-k",
+        config.GUNICORN_WORKER_CLASS,
+        "-w",
+        config.GUNICORN_WORKERS,
+        "-b",
+        config.GUNICORN_BIND,
+        config.GUNICORN_APP_MODULE,
     ]
+
     run()
