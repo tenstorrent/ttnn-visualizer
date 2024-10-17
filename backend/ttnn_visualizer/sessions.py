685658f320f5aad6d52518f7ee2290da6f8868de
Greg Hatt <greg@lightsonsoftware.com>
2024-10-17 15:18:06 -0300

    Adds remote_folder to session data

--- backend/ttnn_visualizer/sessions.py
+++ backend/ttnn_visualizer/sessions.py
@@ -2,43 +2,38 @@ from logging import getLogger
 
 from flask import request, jsonify, current_app
 
-from ttnn_visualizer.models import TabSession
+from ttnn_visualizer.models import RemoteConnection, RemoteFolder, TabSession
 from ttnn_visualizer.extensions import db
 
 logger = getLogger(__name__)
 
 
-def update_tab_session(tab_id, active_report_data, remote_connection_data=None):
+def update_tab_session(tab_id, active_report_data, remote_connection: RemoteConnection =None, remote_folder: RemoteFolder = None):
     """
     Overwrite the active report for a given tab session or create a new session if one doesn't exist.
     Store everything in the database using the TabSession model.
     """
     active_report = {"name": active_report_data.get("name")}
 
-    # Check if remote connection data is provided and add it to the active report
-    remote_connection = None
-    if remote_connection_data:
-        remote_connection = {
-            "name": remote_connection_data.get("name"),
-            "username": remote_connection_data.get("username"),
-            "host": remote_connection_data.get("host"),
-            "port": remote_connection_data.get("port"),
-            "path": remote_connection_data.get("path"),
-        }
 
     # Query the database to find the existing tab session
     session_data = TabSession.query.filter_by(tab_id=tab_id).first()
+    remote_folder_data = remote_folder.dict() if remote_folder else None
+    remote_connection_data = remote_connection.dict() if remote_connection else None
 
     if session_data:
-        # Update the session data
+        session_data.remote_folder = remote_folder_data
+        session_data.remote_connection = remote_connection_data
         session_data.active_report = active_report
-        session_data.remote_connection = remote_connection
+
     else:
         # Create a new session entry
         session_data = TabSession(
             tab_id=tab_id,
             active_report=active_report,
-            remote_connection=remote_connection,
+            remote_connection=remote_connection_data,
+            remote_folder=remote_folder_data
+
         )
 
         db.session.add(session_data)
