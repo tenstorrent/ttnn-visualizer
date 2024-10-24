7dd3bd82cefdad9b9c0087ee54e5dbbd17913b3c
Greg Hatt <greg@lightsonsoftware.com>
2024-10-24 16:18:39 -0300

    Hotfix: Re-adds drop on restart

--- backend/ttnn_visualizer/app.py
+++ backend/ttnn_visualizer/app.py
@@ -80,6 +80,9 @@ def extensions(app: flask.Flask):
     app.config["SESSION_TYPE"] = "sqlalchemy"
     app.config["SESSION_SQLALCHEMY"] = db
 
+    with app.app_context():
+        db.drop_all()
+
     if app.config["USE_WEBSOCKETS"]:
         register_handlers(socketio)
 
