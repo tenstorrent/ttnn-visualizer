3f6b3beafbdc826ab27e0c02fcae81483be3d0be
Greg Hatt <greg@lightsonsoftware.com>
2024-10-24 16:31:46 -0300

    Hotfix: Restore removal of autodrop

--- backend/ttnn_visualizer/app.py
+++ backend/ttnn_visualizer/app.py
@@ -80,9 +80,6 @@ def extensions(app: flask.Flask):
     app.config["SESSION_TYPE"] = "sqlalchemy"
     app.config["SESSION_SQLALCHEMY"] = db
 
-    with app.app_context():
-        db.drop_all()
-
     if app.config["USE_WEBSOCKETS"]:
         register_handlers(socketio)
 
