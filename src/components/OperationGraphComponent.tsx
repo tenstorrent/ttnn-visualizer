42aee7bafbc851e1795550a50b3fe296a2722266
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-11-06 11:55:49 -0500

    usability improvements and feedback for graph viewer

--- src/components/OperationGraphComponent.tsx
+++ src/components/OperationGraphComponent.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Edge, Network } from 'vis-network';
 import { DataSet } from 'vis-data';
 import 'vis-network/styles/vis-network.css';
@@ -12,6 +12,7 @@ import { useNavigate } from 'react-router';
 import { OperationDescription } from '../model/APIData';
 import ROUTES from '../definitions/routes';
 import '../scss/components/OperationGraphComponent.scss';
+import LoadingSpinner from './LoadingSpinner';
 
 type OperationList = OperationDescription[];
 
@@ -22,45 +23,59 @@ const OperationGraph: React.FC<{
     const containerRef = useRef<HTMLDivElement | null>(null);
     const navigate = useNavigate();
     const networkRef = useRef<Network | null>(null);
-
+    const [isLoading, setIsLoading] = useState(true);
     const [scale, setScale] = useState(1);
-    const edges = operationList.flatMap((op) =>
-        op.outputs.flatMap((tensor) =>
-            tensor.consumers.map(
-                (consumerId) =>
-                    ({
-                        from: op.id,
-                        to: consumerId,
-                        arrows: 'to',
-                        label: `${tensor.id}`,
-                    }) as Edge,
+
+    const edges = useMemo(
+        () =>
+            operationList.flatMap((op) =>
+                op.outputs.flatMap((tensor) =>
+                    tensor.consumers.map(
+                        (consumerId) =>
+                            ({
+                                from: op.id,
+                                to: consumerId,
+                                arrows: 'to',
+                                label: `${tensor.id}`,
+                            }) as Edge,
+                    ),
+                ),
             ),
-        ),
+        [operationList],
     );
 
-    const connectedNodeIds = new Set<number>();
-    edges.forEach(({ from, to }) => {
-        connectedNodeIds.add(from as number);
-        connectedNodeIds.add(to as number);
-    });
+    const connectedNodeIds = useMemo(() => {
+        const ids = new Set<number>();
+        edges.forEach(({ from, to }) => {
+            ids.add(from as number);
+            ids.add(to as number);
+        });
+        return ids;
+    }, [edges]);
 
-    const nodes = new DataSet(
-        operationList
-            .filter((op) => connectedNodeIds.has(op.id))
-            .map((op) => ({
-                id: op.id,
-                label: `${op.id} ${op.name}`,
-                shape: 'box',
-            })),
-    );
+    const nodes = useMemo(() => {
+        return new DataSet(
+            operationList
+                .filter((op) => connectedNodeIds.has(op.id))
+                .map((op) => ({
+                    id: op.id,
+                    label: `${op.id} ${op.name}`,
+                    shape: 'box',
+                })),
+        );
+    }, [operationList, connectedNodeIds]);
+
     const focusNodeId = operationId !== undefined ? operationId : (nodes.getIds()[0] as number) ?? 0;
     const [currentOperationId, setCurrentOperationId] = useState(focusNodeId);
 
-    const updateScale = (newScale: number) => {
-        const limitedScale = Math.min(newScale, 3);
-        setScale(limitedScale);
-        networkRef.current?.moveTo({ scale: limitedScale });
-    };
+    const updateScale = useCallback(
+        (newScale: number) => {
+            const limitedScale = Math.min(newScale, 3);
+            setScale(limitedScale);
+            networkRef.current?.moveTo({ scale: limitedScale });
+        },
+        [networkRef],
+    );
 
     // eslint-disable-next-line consistent-return
     useEffect(() => {
@@ -122,6 +137,7 @@ const OperationGraph: React.FC<{
             );
 
             networkRef.current.once('afterDrawing', () => {
+                setIsLoading(false); // Rendering should be complete
                 networkRef.current?.focus(focusNodeId, {
                     scale,
                     animation: { duration: 500, easingFunction: 'easeInOutQuad' },
@@ -178,6 +194,7 @@ const OperationGraph: React.FC<{
                     }
                 }
             });
+            networkRef.current.once('animationFinished', () => {});
 
             return () => {
                 networkRef.current?.off('zoom');
@@ -187,23 +204,26 @@ const OperationGraph: React.FC<{
                 networkRef.current = null;
             };
         }
-        // because we know better and dont want re-renders here.
+        // because we know better and dont want re-renders here we are controlling dependencies.
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [edges, focusNodeId, nodes]);
 
-    const focusOnNode = (nodeId: number | null) => {
-        if (nodeId === null) {
-            return;
-        }
-        if (networkRef.current) {
-            networkRef.current.focus(nodeId, {
-                scale,
-                animation: { duration: 500, easingFunction: 'easeInQuad' },
-            });
-            networkRef.current.selectNodes([nodeId], true);
-            setCurrentOperationId(nodeId);
-        }
-    };
+    const focusOnNode = useCallback(
+        (nodeId: number | null) => {
+            if (nodeId === null) {
+                return;
+            }
+            if (networkRef.current) {
+                networkRef.current.focus(nodeId, {
+                    scale,
+                    animation: { duration: 500, easingFunction: 'easeInOutCubic' },
+                });
+                networkRef.current.selectNodes([nodeId], true);
+                setCurrentOperationId(nodeId);
+            }
+        },
+        [networkRef, scale],
+    );
 
     const getNextOperationId = (currentId: number) => {
         const nodeIds = nodes.getIds();
@@ -234,7 +254,7 @@ const OperationGraph: React.FC<{
                     </Tooltip>
                     <Tooltip
                         placement={PopoverPosition.TOP}
-                        content={`Center on ${currentOperationId}`}
+                        content={`Center on operation ${currentOperationId}`}
                     >
                         <Button
                             outlined
@@ -269,10 +289,17 @@ const OperationGraph: React.FC<{
                     />
                 </div>
             </div>
+            {isLoading && (
+                <div className='loading-overlay'>
+                    <LoadingSpinner />
+                </div>
+            )}
+
             <div
                 ref={containerRef}
-                style={{ width: '100%', height: 'calc(100vh - 180px)' }}
+                style={{ width: '100%', height: 'calc(100vh - 120px)' }}
             />
+
             <div>
                 <aside>
                     Scroll to zoom. Drag to pan. Click a node to see operation details. Click an edge to focus on the
