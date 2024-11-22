edd78f5f694a2c48e5514697d6839651794b77e8
David Blundell <david.blundell@lightsonsoftware.com>
2024-11-22 12:57:26 -0500

    Added selector to tensor list

---
+++ src/components/DeviceSelector.tsx
@@ -0,0 +1,58 @@
+import { Button, Checkbox, MenuItem } from '@blueprintjs/core';
+import { Select } from '@blueprintjs/select';
+import { useAtom } from 'jotai';
+import { IconNames } from '@blueprintjs/icons';
+import { useDevices } from '../hooks/useAPI';
+import { selectedDeviceAtom } from '../store/app';
+import isValidNumber from '../functions/isValidNumber';
+import 'styles/components/DeviceSelector.scss';
+
+function DeviceSelector() {
+    const { data: devices } = useDevices();
+
+    const items = devices ? devices.map((device) => device.device_id) : [];
+    const [selectedDevice, setSelectedDevice] = useAtom(selectedDeviceAtom);
+
+    const updateSelectedDevices = (device?: number) => {
+        setSelectedDevice(device !== selectedDevice ? device : undefined);
+    };
+
+    return (
+        <Select
+            className='device-selector'
+            items={items}
+            itemRenderer={(value) => DeviceItem(value, updateSelectedDevices, selectedDevice)}
+            filterable
+            itemPredicate={(query, device) => device.toString().indexOf(query) >= 0}
+            noResults={
+                <MenuItem
+                    disabled
+                    text='No results.'
+                    roleStructure='listoption'
+                />
+            }
+            onItemSelect={updateSelectedDevices}
+        >
+            <Button
+                text={isValidNumber(selectedDevice) ? `Device ${selectedDevice}` : 'Select device...'}
+                icon={isValidNumber(selectedDevice) ? IconNames.SMALL_SQUARE : null}
+                outlined
+            />
+        </Select>
+    );
+}
+
+const DeviceItem = (device: number, onClick: (device: number) => void, selectedDevice?: number) => {
+    return (
+        <li key={device}>
+            <Checkbox
+                className='buffer-type-checkbox'
+                label={`Device ${device}`}
+                checked={selectedDevice === device}
+                onClick={() => onClick(device)}
+            />
+        </li>
+    );
+};
+
+export default DeviceSelector;
