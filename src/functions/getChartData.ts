7d259d54e9046f952188c56c984a92e0025d8e7a
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-11-25 10:02:21 -0500

    less confusing logic

--- src/functions/getChartData.ts
+++ src/functions/getChartData.ts
@@ -16,12 +16,11 @@ export default function getChartData(
         let color;
         if (overrides?.color) {
             color = overrides?.color;
+        } else if ('color' in chunk && typeof chunk.color === 'string' && chunk.color) {
+            // check for ColoredChunk
+            color = chunk.color;
         } else {
             color = tensorColor !== undefined ? tensorColor : getBufferColor(address + (overrides?.colorVariance || 0));
-            // check for ColoredChunk
-            if ('color' in chunk && typeof chunk.color === 'string') {
-                color = chunk.color;
-            }
         }
 
         return {
