39954d19155dd3c996cfa8fcaebc0e47026eb461
David Blundell <david.blundell@lightsonsoftware.com>
2025-01-28 15:37:38 -0500

    Scoped to one chart with split data

--- src/components/buffer-summary/BufferSummaryPlotRendererDRAM.tsx
+++ src/components/buffer-summary/BufferSummaryPlotRendererDRAM.tsx
@@ -2,12 +2,13 @@
 //
 // SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
-import { UIEvent, useMemo, useRef, useState } from 'react';
+import { Fragment, UIEvent, useMemo, useRef, useState } from 'react';
 import { useVirtualizer } from '@tanstack/react-virtual';
 import classNames from 'classnames';
 import { Switch } from '@blueprintjs/core';
 import { Link } from 'react-router-dom';
 import { useAtom } from 'jotai';
+import { PlotData } from 'plotly.js';
 import { BufferSummaryAxisConfiguration } from '../../definitions/PlotConfigurations';
 import { BuffersByOperationData } from '../../hooks/useAPI';
 import MemoryPlotRenderer from '../operation-details/MemoryPlotRenderer';
@@ -24,6 +25,20 @@ const PLACEHOLDER_ARRAY_SIZE = 30;
 const OPERATION_EL_HEIGHT = 20; // Height in px of each list item
 const TOTAL_SHADE_HEIGHT = 20; // Height in px of 'scroll-shade' pseudo elements
 const MEMORY_ZOOM_PADDING_RATIO = 0.01;
+
+const CHART_DATA: Partial<PlotData>[][] = [
+    [
+        {
+            x: [0],
+            y: [1],
+            type: 'bar',
+            width: [0],
+            marker: {
+                color: 'transparent',
+            },
+        },
+    ],
+];
 
 interface BufferSummaryPlotRendererDRAMProps {
     buffersByOperation: BuffersByOperationData[];
@@ -58,19 +73,21 @@ function BufferSummaryPlotRendererDRAM({
     // TODO: Multi device support
     const memorySize = DRAM_MEMORY_SIZE;
 
-    const zoomedMemoryOptions = segmentedChartData.map((segment) => {
-        const buffers = segment.flatMap((op) => op.buffers);
-        const zoomStart = buffers[0].address;
-        const zoomEnd = buffers[buffers.length - 1].address;
+    const zoomedMemoryOptions = useMemo(
+        () =>
+            segmentedChartData.map((segment) => {
+                const buffers = segment.flatMap((op) => op.buffers);
+                const zoomStart = buffers[0].address;
+                const zoomEnd = buffers[buffers.length - 1].address + buffers[buffers.length - 1].size;
 
-        // console.log('zoomedMemoryOptions', zoomedMemoryOptions);
-
-        return {
-            start: zoomStart,
-            end: zoomEnd,
-            padding: (zoomEnd - zoomStart) * MEMORY_ZOOM_PADDING_RATIO,
-        };
-    });
+                return {
+                    start: zoomStart,
+                    end: zoomEnd,
+                    padding: (zoomEnd - zoomStart) * MEMORY_ZOOM_PADDING_RATIO,
+                };
+            }),
+        [segmentedChartData],
+    );
 
     const virtualizer = useVirtualizer({
         count: buffersByOperation?.length || PLACEHOLDER_ARRAY_SIZE,
@@ -117,96 +134,79 @@ function BufferSummaryPlotRendererDRAM({
 
             <p className='x-axis-label'>Memory Address</p>
 
-            <div style={{ display: 'flex', flexWrap: 'nowrap', gap: '4rem' }}>
-                {segmentedChartData.map((segment, index) => (
-                    <div
-                        key={`${segment[index].name}-${index}`}
-                        style={{ flexBasis: '50%' }}
-                    >
-                        <div className='chart-position'>
-                            <MemoryPlotRenderer
-                                className='buffer-summary-plot'
-                                chartDataList={[
-                                    [
-                                        {
-                                            x: [0],
-                                            y: [1],
-                                            type: 'bar',
-                                            width: [0],
-                                            marker: {
-                                                color: 'transparent',
-                                            },
-                                        },
-                                    ],
-                                ]}
-                                isZoomedIn={isZoomedIn}
-                                memorySize={isZoomedIn ? zoomedMemoryOptions[index].end : memorySize}
-                                plotZoomRange={
-                                    isZoomedIn
-                                        ? [
-                                              zoomedMemoryOptions[index].start - zoomedMemoryOptions[index].padding,
-                                              zoomedMemoryOptions[index].end + zoomedMemoryOptions[index].padding,
-                                          ]
-                                        : [0, memorySize]
-                                }
-                                configuration={BufferSummaryAxisConfiguration}
-                            />
-                        </div>
+            {segmentedChartData.slice(0, 1).map((segment, index) => (
+                <Fragment key={`${segment[index].name}-${index}`}>
+                    <div className='chart-position'>
+                        <MemoryPlotRenderer
+                            className='buffer-summary-plot'
+                            chartDataList={CHART_DATA}
+                            isZoomedIn={isZoomedIn}
+                            memorySize={isZoomedIn ? zoomedMemoryOptions[index].end : memorySize}
+                            plotZoomRange={
+                                isZoomedIn
+                                    ? [
+                                          zoomedMemoryOptions[index].start - zoomedMemoryOptions[index].padding,
+                                          zoomedMemoryOptions[index].end + zoomedMemoryOptions[index].padding,
+                                      ]
+                                    : [0, memorySize]
+                            }
+                            configuration={BufferSummaryAxisConfiguration}
+                        />
+                    </div>
 
+                    <div
+                        className={classNames('scrollable-element', {
+                            'scroll-shade-top': hasScrolledFromTop,
+                            'scroll-shade-bottom': !hasScrolledToBottom && numberOfOperations > virtualItems.length,
+                        })}
+                        onScroll={(event) => handleUserScrolling(event)}
+                        ref={scrollElementRef}
+                    >
                         <div
-                            className={classNames('scrollable-element', {
-                                'scroll-shade-top': hasScrolledFromTop,
-                                'scroll-shade-bottom': !hasScrolledToBottom && numberOfOperations > virtualItems.length,
-                            })}
-                            onScroll={(event) => handleUserScrolling(event)}
-                            ref={scrollElementRef}
+                            style={{
+                                // Div is sized to the maximum required to render all list items minus our shade element heights
+                                height: virtualizer.getTotalSize() - TOTAL_SHADE_HEIGHT,
+                            }}
                         >
                             <div
+                                className='list-container'
                                 style={{
-                                    // Div is sized to the maximum required to render all list items minus our shade element heights
-                                    height: virtualizer.getTotalSize() - TOTAL_SHADE_HEIGHT,
+                                    // Tracks scroll position
+                                    transform: `translateY(${virtualItems[0]?.start ?? 0}px)`,
                                 }}
                             >
-                                <div
-                                    className='list-container'
-                                    style={{
-                                        // Tracks scroll position
-                                        transform: `translateY(${virtualItems[0]?.start ?? 0}px)`,
-                                    }}
-                                >
-                                    {virtualItems.map((virtualRow) => {
-                                        const operation = segment[virtualRow.index];
+                                {virtualItems.map((virtualRow) => {
+                                    const operation = segment[virtualRow.index];
 
-                                        return (
-                                            <div
-                                                className='buffer-summary-plot-container'
-                                                key={virtualRow.key}
-                                                data-index={virtualRow.index}
-                                                ref={virtualizer.measureElement}
+                                    return operation ? (
+                                        <div
+                                            className='buffer-summary-plot-container'
+                                            key={virtualRow.key}
+                                            data-index={virtualRow.index}
+                                            ref={virtualizer.measureElement}
+                                        >
+                                            <BufferSummaryRow
+                                                buffers={operation.buffers}
+                                                // operationId={operation.id}
+                                                memoryStart={isZoomedIn ? zoomedMemoryOptions[index].start : 0}
+                                                memoryEnd={isZoomedIn ? zoomedMemoryOptions[index].end : memorySize}
+                                                memoryPadding={zoomedMemoryOptions[index].padding}
+                                                tensorList={tensorListByOperation.get(operation.id)!}
+                                            />
+                                            <Link
+                                                to={`${ROUTES.OPERATIONS}/${operation.id}`}
+                                                className='y-axis-tick'
                                             >
-                                                <BufferSummaryRow
-                                                    buffers={operation.buffers}
-                                                    operationId={operation.id}
-                                                    memoryStart={isZoomedIn ? zoomedMemoryOptions[index].start : 0}
-                                                    memoryEnd={isZoomedIn ? zoomedMemoryOptions[index].end : memorySize}
-                                                    memoryPadding={zoomedMemoryOptions[index].padding}
-                                                    tensorList={tensorListByOperation.get(operation.id)!}
-                                                />
-                                                <Link
-                                                    to={`${ROUTES.OPERATIONS}/${operation.id}`}
-                                                    className='y-axis-tick'
-                                                >
-                                                    {operation.id}
-                                                </Link>
-                                            </div>
-                                        );
-                                    })}
-                                </div>
+                                                {operation.id}
+                                            </Link>
+                                        </div>
+                                    ) : null;
+                                })}
                             </div>
                         </div>
                     </div>
-                ))}
-            </div>
+                </Fragment>
+            ))}
         </div>
     ) : (
         <LoadingSpinner />
