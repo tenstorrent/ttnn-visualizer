def3755278097cb3f438e2cf90be1dd06622ef0d
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-01-29 09:17:50 -0500

    tensor selection

--- src/components/buffer-summary/BufferSummaryRow.tsx
+++ src/components/buffer-summary/BufferSummaryRow.tsx
@@ -5,9 +5,12 @@
 import 'styles/components/BufferSummaryRow.scss';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { PopoverPosition, Tooltip } from '@blueprintjs/core';
+import { useAtom } from 'jotai/index';
 import { Buffer, Tensor } from '../../model/APIData';
 import { getBufferColor, getTensorColor } from '../../functions/colorGenerator';
 import { formatSize, toHex, toReadableShape, toReadableType } from '../../functions/math';
+import { selectedAddressAtom, selectedTensorAtom } from '../../store/app';
+import useBufferFocus from '../../hooks/useBufferFocus';
 
 interface BufferSummaryRowProps {
     buffers: Buffer[];
@@ -18,23 +21,30 @@ interface BufferSummaryRowProps {
 }
 
 const SCALE = 100;
+const CANVAS_WIDTH = 1200;
+const CANVAS_HEIGHT = 20;
+const TARGET_SCALE = (CANVAS_WIDTH / SCALE) * 100;
 
-function BufferSummaryRow({ buffers, memoryStart, memoryEnd, memoryPadding, tensorList }: BufferSummaryRowProps) {
+const BufferSummaryRow = ({ buffers, memoryStart, memoryEnd, memoryPadding, tensorList }: BufferSummaryRowProps) => {
     const computedMemorySize = memoryEnd - memoryStart;
     const computedPadding = (memoryPadding / computedMemorySize) * SCALE;
-    const CANVAS_WIDTH = 1200;
-    const CANVAS_HEIGHT = 20;
+
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const targetScale = (CANVAS_WIDTH / SCALE) * 100;
     const [tooltip, setTooltip] = useState<{ x: number; y: number; text: React.JSX.Element } | null>(null);
+    const [selectedTensor, setSelectedTensor] = useAtom(selectedTensorAtom);
+    const [, setSelectedAddress] = useAtom(selectedAddressAtom);
+    const { createToast, resetToasts } = useBufferFocus();
+
     const interactivityList = useMemo(() => {
         return buffers.map((buffer) => {
-            const size = (buffer.size / computedMemorySize) * targetScale;
-            const position = ((buffer.address - memoryStart) / computedMemorySize) * targetScale;
+            const size = (buffer.size / computedMemorySize) * TARGET_SCALE;
+            const position = ((buffer.address - memoryStart) / computedMemorySize) * TARGET_SCALE;
             const tensor = tensorList.get(buffer.address);
-            return { position, size, tensor, buffer };
+            const color = (tensor ? getTensorColor(tensor.id) : getBufferColor(buffer.address)) || 'black';
+            return { position, size, tensor, color, buffer };
         });
-    }, [buffers, computedMemorySize, memoryStart, targetScale, tensorList]);
+    }, [buffers, computedMemorySize, memoryStart, tensorList]);
+
     useEffect(() => {
         const canvas = canvasRef.current;
         if (canvas) {
@@ -42,63 +52,90 @@ function BufferSummaryRow({ buffers, memoryStart, memoryEnd, memoryPadding, tens
             if (ctx) {
                 ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-                interactivityList.forEach(({ position, size, tensor, buffer }) => {
-                    ctx.fillStyle = (tensor ? getTensorColor(tensor.id) : getBufferColor(buffer.address)) || 'black';
+                interactivityList.forEach(({ color, position, size }) => {
+                    ctx.fillStyle = color;
                     ctx.fillRect(position, 1, size, CANVAS_HEIGHT);
                 });
             }
         }
-    }, [interactivityList, CANVAS_HEIGHT, CANVAS_WIDTH]);
-    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    }, [interactivityList]);
+
+    const findBufferForInteraction = (event: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
         if (canvas) {
             const rect = canvas.getBoundingClientRect();
             const scaleX = CANVAS_WIDTH / rect.width;
             const mouseX = (event.clientX - rect.left) * scaleX;
-            const interactiveBuffer = interactivityList.find(
-                (b) => mouseX >= b.position && mouseX <= b.position + b.size,
-            );
+            return {
+                interactiveBuffer: interactivityList.find((b) => mouseX >= b.position && mouseX <= b.position + b.size),
+                scaleX,
+            };
+        }
+        return {};
+    };
 
-            if (interactiveBuffer) {
-                const x = interactiveBuffer.position / scaleX;
-                const color =
-                    (interactiveBuffer.tensor
-                        ? getTensorColor(interactiveBuffer.tensor.id)
-                        : getBufferColor(interactiveBuffer.buffer.address)) || 'black';
-                setTooltip({
-                    x,
-                    y: 0,
-                    text: (
-                        <div>
-                            <strong>
-                                <span style={{ fontSize: '20px', color, marginRight: '2px' }}>&#9632;</span>
-                                {interactiveBuffer.buffer.address} ({toHex(interactiveBuffer.buffer.address)})<br />
-                                Size: {formatSize(interactiveBuffer.buffer.size)}
-                                <br />
-                                {interactiveBuffer.tensor?.shape
-                                    ? toReadableShape(interactiveBuffer.tensor.shape)
-                                    : ''}{' '}
-                                {interactiveBuffer.tensor?.dtype ? toReadableType(interactiveBuffer.tensor.dtype) : ''}{' '}
-                                {interactiveBuffer.tensor?.id ? `Tensor ${interactiveBuffer.tensor.id}` : ''}
-                                {interactiveBuffer.tensor?.memory_config?.memory_layout ? (
-                                    <>
-                                        <br />
-                                        {interactiveBuffer.tensor?.memory_config?.memory_layout}
-                                    </>
-                                ) : null}
-                            </strong>
-                        </div>
-                    ),
-                });
-            } else {
-                setTooltip(null);
-            }
+    const handleClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const { interactiveBuffer } = findBufferForInteraction(event);
+        if (interactiveBuffer) {
+            setFocusedBuffer(interactiveBuffer.buffer, interactiveBuffer.tensor);
+        } else {
+            clearFocusedBuffer();
+        }
+    };
+    const clearFocusedBuffer = () => {
+        resetToasts();
+    };
+
+    const setFocusedBuffer = (buffer: Buffer, tensor?: Tensor) => {
+        if (!buffer) {
+            clearFocusedBuffer();
+            return;
+        }
+        setSelectedTensor(tensor?.id === selectedTensor ? null : tensor?.id ?? null);
+        setSelectedAddress(tensor?.address === selectedTensor ? null : tensor?.address ?? buffer.address);
+        createToast(tensor?.address ?? buffer.address, tensor?.id);
+    };
+
+    const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const { interactiveBuffer, scaleX } = findBufferForInteraction(event);
+        if (interactiveBuffer) {
+            const x = interactiveBuffer.position / scaleX;
+            const { color } = interactiveBuffer;
+
+            setTooltip({
+                x,
+                y: 0,
+                text: (
+                    <div>
+                        <strong>
+                            <span style={{ fontSize: '20px', color, marginRight: '2px' }}>&#9632;</span>
+                            {interactiveBuffer.buffer.address} ({toHex(interactiveBuffer.buffer.address)})<br />
+                            Size: {formatSize(interactiveBuffer.buffer.size)}
+                            <br />
+                            {interactiveBuffer.tensor?.shape
+                                ? toReadableShape(interactiveBuffer.tensor.shape)
+                                : ''}{' '}
+                            {interactiveBuffer.tensor?.dtype ? toReadableType(interactiveBuffer.tensor.dtype) : ''}{' '}
+                            {interactiveBuffer.tensor?.id ? `Tensor ${interactiveBuffer.tensor.id}` : ''}
+                            {interactiveBuffer.tensor?.memory_config?.memory_layout ? (
+                                <>
+                                    <br />
+                                    {interactiveBuffer.tensor?.memory_config?.memory_layout}
+                                </>
+                            ) : null}
+                        </strong>
+                    </div>
+                ),
+            });
+        } else {
+            setTooltip(null);
         }
     };
 
     const handleMouseLeave = () => {
         setTooltip(null);
     };
+
     return (
         <>
             {tooltip && (
@@ -146,10 +183,11 @@ function BufferSummaryRow({ buffers, memoryStart, memoryEnd, memoryPadding, tens
                     height={CANVAS_HEIGHT}
                     onMouseMove={handleMouseMove}
                     onMouseLeave={handleMouseLeave}
+                    onClick={handleClick}
                 />
             </div>
         </>
     );
-}
+};
 
 export default BufferSummaryRow;
