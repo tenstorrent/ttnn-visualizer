b0e4003622501f9daf106bbcd5af20b0a1c6b5d4
David Blundell <david.blundell@lightsonsoftware.com>
2025-01-31 13:05:22 -0500

    Add range support to buffers

--- src/routes/BufferSummary.tsx
+++ src/routes/BufferSummary.tsx
@@ -16,7 +16,6 @@ import BufferSummaryTab from '../components/buffer-summary/BufferSummaryTab';
 import LoadingSpinner from '../components/LoadingSpinner';
 import 'styles/components/BufferSummary.scss';
 import { SECTION_IDS } from '../definitions/BufferSummary';
-import Range from '../components/RangeSlider';
 
 enum TAB_IDS {
     L1 = 'L1',
@@ -69,8 +68,6 @@ function BufferSummary() {
             <Helmet title='Buffer summary' />
 
             <h1 className='page-title'>Buffers by operation</h1>
-
-            <Range />
 
             <ButtonGroup className='sticky-nav'>
                 <AnchorButton
