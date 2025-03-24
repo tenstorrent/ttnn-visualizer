2a8acbe117cb0891d4a202373c6eae042ed67f38
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-03-24 15:27:42 -0400

    parse url to set instance id if its provided

--- src/libs/axiosInstance.ts
+++ src/libs/axiosInstance.ts
@@ -11,6 +11,11 @@ const axiosInstance = axios.create({
 
 export const getOrCreateInstanceId = () => {
     let instanceId = sessionStorage.getItem('instanceId');
+    const urlInstanceId = new URLSearchParams(window.location.search).get('instanceId');
+    if (urlInstanceId) {
+        instanceId = urlInstanceId;
+        sessionStorage.setItem('instanceId', instanceId);
+    }
     if (!instanceId) {
         instanceId = Math.random().toString(36).substring(2, 15);
         sessionStorage.setItem('instanceId', instanceId);
