ca0ac3f281d24953c256d6d50da5a94b21450976
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-03-25 13:07:08 -0400

    make logic more clear, encapsulate the loop

--- src/definitions/L1MemorySize.ts
+++ src/definitions/L1MemorySize.ts
@@ -1,1 +1,1 @@
-export const L1_DEFAULT_MEMORY_SIZE = 1397024; // 1.34MB this is a fallback value only for when there is no devices information
+export const L1_DEFAULT_MEMORY_SIZE = 1499136; // 1.43MB   this is a fallback value only for when there is no devices information
