82b35ad594995813077df0f8dd94c655239c3673
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-04-23 14:28:18 -0400

    git push command for the release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -32,4 +32,6 @@ console.log(`Updated pyproject.toml to version ${newVersion}`);
 execSync(`git add package.json pyproject.toml`, { stdio: 'inherit' });
 execSync(`git commit -m "v${newVersion}"`, { stdio: 'inherit' });
 execSync(`git tag v${newVersion}`, { stdio: 'inherit' });
+execSync(`git push origin v${newVersion}`, { stdio: 'inherit' });
+
 console.log(`Committed and tagged as v${newVersion}`);
