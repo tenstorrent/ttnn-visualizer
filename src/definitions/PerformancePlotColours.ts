28a33cd451572a19d54e65e652b00e61dec4f589
David Blundell <david.blundell@lightsonsoftware.com>
2025-04-30 09:55:14 -0400

    Changed plot colours

--- src/definitions/PerformancePlotColours.ts
+++ src/definitions/PerformancePlotColours.ts
@@ -2,11 +2,11 @@
 //
 // SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 
-// orange and blue
-const PRIMARY_COLOURS = ['#E27414', '#008BF8'];
+// blue and green
+const PRIMARY_COLOURS = ['#008BF8', '#58B73B'];
 
-// red and green
-const SECONDARY_COLOURS = ['#FF0400', '#7DE200'];
+// orange and yellow
+const SECONDARY_COLOURS = ['#E27414', '#E8C547'];
 
 export const getPrimaryDataColours = (index: number) => PRIMARY_COLOURS[index];
 export const getSecondaryDataColours = (index: number) => SECONDARY_COLOURS[index];
