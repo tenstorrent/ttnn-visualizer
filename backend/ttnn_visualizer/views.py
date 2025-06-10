b35715e76eeb4b667c8552c5681b1ec6832fcd77
Scott Mountenay <ctr-smountenay@ext.tenstorrent.com>
2025-06-10 12:20:22 -0600

    Fix getting performance report with filename prefixes

--- backend/ttnn_visualizer/views.py
+++ backend/ttnn_visualizer/views.py
@@ -581,13 +581,6 @@ def get_performance_results_report(instance: Instance):
     if not instance.performance_path:
         return Response(status=HTTPStatus.NOT_FOUND)
 
-    name = request.args.get("name", None)
-    performance_path = Path(instance.performance_path)
-    if name:
-        performance_path = performance_path.parent / name
-        instance.performance_path = str(performance_path)
-        logger.info(f"************ Performance path set to {instance.performance_path}")
-
     try:
         report = OpsPerformanceReportQueries.generate_report(instance)
     except DataFormatError:
