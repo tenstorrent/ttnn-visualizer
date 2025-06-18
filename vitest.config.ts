fa693eb77efc412fa6c7185da51cd27b57b65c9e
David Blundell <david.blundell@lightsonsoftware.com>
2025-06-18 18:08:42 -0400

    Merging configs for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,33 +2,39 @@
 //
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 import path from 'path';
+import viteConfig from './vite.config';
 
-export default defineConfig({
-    test: {
-        setupFiles: ['./vitest.setup.ts'],
-        environment: 'jsdom',
-        deps: {
-            optimizer: {
-                web: {
-                    include: ['vitest-canvas-mock'],
+export default defineConfig((configEnv) =>
+    mergeConfig(
+        viteConfig(configEnv),
+        defineConfig({
+            test: {
+                setupFiles: ['./vitest.setup.ts'],
+                environment: 'jsdom',
+                deps: {
+                    optimizer: {
+                        web: {
+                            include: ['vitest-canvas-mock'],
+                        },
+                    },
+                },
+                poolOptions: {
+                    threads: {
+                        singleThread: true,
+                    },
+                },
+                environmentOptions: {
+                    jsdom: {
+                        resources: 'usable',
+                    },
+                },
+                alias: {
+                    'styles/': `${path.resolve(__dirname, 'src/scss')}/`,
+                    '@blueprintjs': path.resolve(__dirname, './node_modules/@blueprintjs'),
                 },
             },
-        },
-        poolOptions: {
-            threads: {
-                singleThread: true,
-            },
-        },
-        environmentOptions: {
-            jsdom: {
-                resources: 'usable',
-            },
-        },
-        alias: {
-            'styles/': `${path.resolve(__dirname, 'src/scss')}/`,
-            '@blueprintjs': path.resolve(__dirname, './node_modules/@blueprintjs'),
-        },
-    },
-});
+        }),
+    ),
+);
