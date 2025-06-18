d43e6a4432482a7ba88104dc277bf46f646e81e8
David Blundell <david.blundell@lightsonsoftware.com>
2025-06-18 17:39:33 -0400

    Added license headers

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 import { defineConfig } from 'vitest/config';
 import path from 'path';
 
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 import { vi } from 'vitest';
 import 'vitest-canvas-mock';
 
