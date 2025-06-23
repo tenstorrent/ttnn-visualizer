13f912128821a288ceaac5b2fff562dd00780229
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-06-23 11:39:41 -0400

    NPE demo reports updated and cleaned up

--- src/routes/NPE.tsx
+++ src/routes/NPE.tsx
@@ -6,7 +6,9 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useAtomValue } from 'jotai';
-import { Button } from '@blueprintjs/core';
+import { Button, ButtonVariant, MenuItem } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
+import { ItemRenderer, Select } from '@blueprintjs/select';
 import NPEFileLoader from '../components/npe/NPEFileLoader';
 import NPEView from '../components/npe/NPEViewComponent';
 import { useNpe } from '../hooks/useAPI';
@@ -30,19 +32,51 @@ const NPE_REPO_URL = (
     </a>
 );
 
+enum NPEDemo {
+    SINGLE_CHIP = 'singlechip',
+    MULTI_CHIP = 'multichip',
+}
+
+interface NPEDemoData {
+    reportFile: NPEDemo;
+    label: string;
+    data: NPEData;
+}
+
+const NPE_DEMO_DATA: NPEDemoData[] = [
+    {
+        reportFile: NPEDemo.SINGLE_CHIP,
+        data: npeDemoDataSinglechip as unknown as NPEData,
+        label: 'NPE single chip demo',
+    },
+    {
+        reportFile: NPEDemo.MULTI_CHIP,
+        data: npeDemoDataMultichip as unknown as NPEData,
+        label: 'NPE multichip demo',
+    },
+];
+
 const NPE: React.FC = () => {
     const npeFileName = useAtomValue(activeNpeOpTraceAtom);
     const { data: loadedData, isLoading } = useNpe(npeFileName);
     const [demoData, setDemoData] = useState<NPEData | null>(null);
+    const [selectedDemo, setSelectedDemo] = useState<NPEDemoData | null>(null);
 
-    const loadNPEDemoReport = (type: 'singleChip' | 'multichip') => {
-        const data: NPEData =
-            type === 'multichip'
-                ? (npeDemoDataMultichip as unknown as NPEData)
-                : (npeDemoDataSinglechip as unknown as NPEData);
-        setDemoData(data);
+    const renderItem: ItemRenderer<NPEDemoData> = (item: NPEDemoData, { handleClick, modifiers }) => {
+        return (
+            <MenuItem
+                key={item.reportFile}
+                textClassName='folder-picker-label'
+                text={item.label}
+                labelClassName='folder-picker-name-label'
+                active={item.reportFile === selectedDemo?.reportFile}
+                roleStructure='listoption'
+                disabled={modifiers.disabled}
+                onClick={handleClick}
+                icon={IconNames.SAVED}
+            />
+        );
     };
-
     const npeData = demoData || loadedData;
     const isDemoEnabled = getServerConfig()?.SERVER_MODE;
     return (
@@ -52,6 +86,49 @@ const NPE: React.FC = () => {
             <h1 className='page-title'>NOC performance estimator</h1>
 
             <NPEFileLoader />
+            {!loadedData && isDemoEnabled && (
+                <>
+                    <Select
+                        className=''
+                        items={NPE_DEMO_DATA}
+                        itemRenderer={renderItem}
+                        noResults={
+                            <MenuItem
+                                disabled
+                                text='No results.'
+                                roleStructure='listoption'
+                            />
+                        }
+                        onItemSelect={(item) => {
+                            const { data } = item;
+                            setSelectedDemo(item);
+                            setDemoData(data);
+                        }}
+                    >
+                        {selectedDemo ? (
+                            <Button
+                                className='folder-picker-button'
+                                text={selectedDemo.label}
+                                alignText='start'
+                                icon={IconNames.SAVED}
+                                endIcon={IconNames.CARET_DOWN}
+                                variant={ButtonVariant.OUTLINED}
+                            />
+                        ) : (
+                            <Button
+                                className='folder-picker-button'
+                                text='Select a demo NPE report'
+                                alignText='start'
+                                icon={IconNames.DOCUMENT_OPEN}
+                                endIcon={IconNames.CARET_DOWN}
+                                variant={ButtonVariant.OUTLINED}
+                            />
+                        )}
+                    </Select>
+
+                    <br />
+                </>
+            )}
 
             {isLoading ? (
                 <LoadingSpinner />
@@ -79,29 +156,7 @@ const NPE: React.FC = () => {
                     </>
                 )
             ) : (
-                <>
-                    <p>Please upload a NPE file for analysis.</p>
-                    <p>See {NPE_REPO_URL} for details on how to generate NPE files.</p>
-                    {isDemoEnabled && (
-                        <>
-                            <Button
-                                onClick={() => {
-                                    loadNPEDemoReport('singleChip');
-                                }}
-                            >
-                                NPE single chip demo
-                            </Button>
-                            <br />
-                            <Button
-                                onClick={() => {
-                                    loadNPEDemoReport('multichip');
-                                }}
-                            >
-                                NPE multichip demo
-                            </Button>
-                        </>
-                    )}
-                </>
+                <p>See {NPE_REPO_URL} for details on how to generate NPE report files.</p>
             )}
         </>
     );
