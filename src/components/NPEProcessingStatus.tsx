f67f7c5985f807f6fb4104f3ae946965544b1b4c
David Blundell <david.blundell@lightsonsoftware.com>
2025-07-28 15:19:51 -0400

    Moved messaging to a new component and improved copy and styling.

---
+++ src/components/NPEProcessingStatus.tsx
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+import { Callout, Intent } from '@blueprintjs/core';
+import 'styles/components/NPEProcessingStatus.scss';
+
+const NPE_REPO_URL = (
+    <a
+        target='_blank'
+        href='https://github.com/tenstorrent/tt-npe'
+        rel='noreferrer'
+    >
+        tt-npe
+    </a>
+);
+
+interface NPEProcessingStatusProps {
+    matchedVersion?: string;
+    expectedVersion: string;
+    npeData?: {
+        common_info?: {
+            version?: string;
+        };
+    };
+}
+
+const NPEProcessingStatus = ({ matchedVersion, expectedVersion, npeData }: NPEProcessingStatusProps) => {
+    return (
+        <Callout
+            intent={Intent.WARNING}
+            title={matchedVersion ? 'Invalid NPE version' : 'Invalid NPE data'}
+            className='npe-processing-status'
+        >
+            {matchedVersion ? (
+                <>
+                    <p>
+                        Current supported version is <u>{expectedVersion}</u>, uploaded data version is{' '}
+                        <u>{npeData?.common_info?.version || 'null'}</u>.
+                    </p>
+
+                    <p>
+                        Use {NPE_REPO_URL} to generate new NPE dataset or install an older version of the visualizer
+                        <code className='formatted-code'>
+                            {matchedVersion ? `pip install ttnn-visualizer==${matchedVersion}` : null}
+                        </code>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <p>Unable to process uploaded NPE data.</p>
+                    <p>Use {NPE_REPO_URL} to generate a new dataset.</p>
+                </>
+            )}
+        </Callout>
+    );
+};
+
+export default NPEProcessingStatus;
