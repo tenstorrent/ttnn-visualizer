fc0b957a2d010a30b4b29737d6ef94bc0d0cec7e
David Blundell <david.blundell@lightsonsoftware.com>
2025-07-31 16:33:12 -0400

    Removed unnecessary changes

--- backend/ttnn_visualizer/sftp_operations.py
+++ backend/ttnn_visualizer/sftp_operations.py
@@ -623,18 +623,13 @@ def read_remote_file(
 
 @remote_exception_handler
 def check_remote_path_for_reports(remote_connection):
-    remote_profiler_paths = []
-    remote_performance_paths = []
+    remote_profiler_paths = find_folders_by_files(
+        remote_connection, remote_connection.profilerPath, [TEST_CONFIG_FILE]
+    )
 
-    if getattr(remote_connection, "profilerPath", None):
-        remote_profiler_paths = find_folders_by_files(
-            remote_connection, remote_connection.profilerPath, [TEST_CONFIG_FILE]
-        )
-
-    if getattr(remote_connection, "performancePath", None):
-        remote_performance_paths = find_folders_by_files(
-            remote_connection, remote_connection.performancePath, [TEST_PROFILER_FILE]
-        )
+    remote_performance_paths = find_folders_by_files(
+        remote_connection, remote_connection.performancePath, [TEST_PROFILER_FILE]
+    )
 
     if not remote_profiler_paths and not remote_performance_paths:
         raise NoProjectsException(
