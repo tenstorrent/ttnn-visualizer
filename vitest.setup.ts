a0e06898663db014fed1b4d3c2b94d5c2cb6ea22
David Blundell <david.blundell@lightsonsoftware.com>
2025-08-05 15:23:56 -0400

    Updated const naming and comments

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -12,21 +12,23 @@ if (!window.URL) {
 window.URL.createObjectURL = vi.fn();
 
 // JSDOM can't process this image from Plotly.js so we are mocking it here to avoid the warning noise in the logs
-const originalSrc = Object.getOwnPropertyDescriptor(window.HTMLImageElement.prototype, 'src');
+const ORIGINAL_SRC = Object.getOwnPropertyDescriptor(window.HTMLImageElement.prototype, 'src');
+
+// This is the base64-encoded WebP image data used by Plotly.js for feature detection.
+// See: https://github.com/plotly/mapbox-gl-js/blob/master/src/util/webp_supported.js
+const PLOTLY_WEBP_TEST_IMAGE_SRC = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=';
 
 Object.defineProperty(window.HTMLImageElement.prototype, 'src', {
     set(src: string) {
-        if (src === 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=') {
-            // Simulate a successful load
+        if (src === PLOTLY_WEBP_TEST_IMAGE_SRC) {
             setTimeout(() => {
                 if (typeof this.onload === 'function') {
                     this.onload(new Event('load'));
                 }
             }, 0);
-        } else if (originalSrc && originalSrc.set) {
-            // Fallback to the original setter for all other images
-            originalSrc.set.call(this, src);
+        } else if (ORIGINAL_SRC?.set) {
+            ORIGINAL_SRC.set.call(this, src);
         }
     },
-    get: originalSrc?.get,
+    get: ORIGINAL_SRC?.get,
 });
