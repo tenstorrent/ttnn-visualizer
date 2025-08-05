1cd8914db52008bd43569f121241616c42f152e0
David Blundell <david.blundell@lightsonsoftware.com>
2025-08-05 15:16:43 -0400

    Avoid JSDOM webp warning

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -10,3 +10,23 @@ if (!window.URL) {
     window.URL = {} as any;
 }
 window.URL.createObjectURL = vi.fn();
+
+// JSDOM can't process this image from Plotly.js so we are mocking it here to avoid the warning noise in the logs
+const originalSrc = Object.getOwnPropertyDescriptor(window.HTMLImageElement.prototype, 'src');
+
+Object.defineProperty(window.HTMLImageElement.prototype, 'src', {
+    set(src: string) {
+        if (src === 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=') {
+            // Simulate a successful load
+            setTimeout(() => {
+                if (typeof this.onload === 'function') {
+                    this.onload(new Event('load'));
+                }
+            }, 0);
+        } else if (originalSrc && originalSrc.set) {
+            // Fallback to the original setter for all other images
+            originalSrc.set.call(this, src);
+        }
+    },
+    get: originalSrc?.get,
+});
