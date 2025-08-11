f84e4371e681ee41ab92e2b86b516b104f20e3ba
David Blundell <david.blundell@lightsonsoftware.com>
2025-08-11 12:51:24 -0400

    Add header

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 import { ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import ROUTES from '../definitions/Routes';
