562f7ce2ab612f43cf5520d93f12077b4ff2cdbf
David Blundell <david.blundell@lightsonsoftware.com>
2025-09-08 11:53:53 -0400

    Add missing SPDX header

--- src/functions/stackedPerfFunctions.tsx
+++ src/functions/stackedPerfFunctions.tsx
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 import React from 'react';
 import HighlightedText from '../components/HighlightedText';
 import { formatPercentage, formatSize } from './math';
