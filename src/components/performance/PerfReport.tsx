737813967271aa24d71d905ec6a9c8d79a4c90d5
David Blundell <ctr-dblundell@tenstorrent.com>
2025-11-11 16:24:08 -0500

    Added compact prop

--- src/components/performance/PerfReport.tsx
+++ src/components/performance/PerfReport.tsx
@@ -465,6 +465,7 @@ const PerformanceReport: FC<PerformanceReportProps> = ({
                     className='multi-device-note'
                     intent={Intent.PRIMARY}
                     icon={IconNames.INFO_SIGN}
+                    compact
                 >
                     Multi device operations are merged into single rows using <u>average duration</u> for collective
                     operations (AllGather, ReduceScatter, AllReduce) and <u>maximum duration</u> for all others.
