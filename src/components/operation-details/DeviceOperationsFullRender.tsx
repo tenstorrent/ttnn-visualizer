a64d7d70edb62fb6a5536b99e59f303785ee42f5
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2025-12-10 11:16:06 -0500

    Removed unused tensor size fetching to speedup the load
    Removed the use of useGetTensorSizesById and related input processing in DeviceOperationsFullRender and processMemoryAllocations, as they are currently not needed. The code is commented out for potential future use.

--- src/components/operation-details/DeviceOperationsFullRender.tsx
+++ src/components/operation-details/DeviceOperationsFullRender.tsx
@@ -17,7 +17,6 @@ import { AllocationDetails, processMemoryAllocations } from '../../functions/pro
 import { formatSize, prettyPrintAddress, toReadableShape } from '../../functions/math';
 import { getBufferColor, getTensorColor } from '../../functions/colorGenerator';
 import MemoryTag from '../MemoryTag';
-import { useGetTensorSizesById } from '../../hooks/useAPI';
 import { L1_DEFAULT_MEMORY_SIZE, L1_NUM_CORES } from '../../definitions/L1MemorySize';
 
 // TODO: this component definitely needs to be broken down into smaller components
@@ -28,10 +27,11 @@ const DeviceOperationsFullRender: React.FC<{
     onLegendClick: (address: number, tensorId?: number) => void;
 }> = ({ deviceOperations, details, onLegendClick }) => {
     const selectedAddress = useAtomValue(selectedAddressAtom);
-
-    const inputIds = details.inputs.map((tensor) => tensor?.id);
-    const inputs = useGetTensorSizesById(inputIds);
-    const { memoryAllocationList, peakMemoryLoad } = processMemoryAllocations(deviceOperations, inputs);
+    // We dont need this for now so commenting out
+    // i would liek this to stay as this might comeback as needed
+    // const inputIds = details.inputs.map((tensor) => tensor?.id);
+    // const inputs = useGetTensorSizesById(inputIds);
+    const { memoryAllocationList, peakMemoryLoad } = processMemoryAllocations(deviceOperations);
 
     const formatDeviceOpParameters = useCallback(
         (node: Node) => {
